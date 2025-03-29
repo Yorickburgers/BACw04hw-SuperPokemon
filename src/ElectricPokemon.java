0437186dd697e7e7458f1172febf6f161f001518
yorickburgers <yorickburgers@live.nl>
2025-03-29 15:51:41 +0100

    made ElectricPokemon sub-class

---
+++ src/ElectricPokemon.java
@@ -0,0 +1,54 @@
+public class ElectricPokemon extends Pokemon{
+    //variables
+    private String type;
+    private String weakTo;
+    private String strongAgainst;
+
+    //constructor
+    public ElectricPokemon(String name, int hp, int level) {
+        super(name, hp, level);
+        this.type = "Electric";
+        this.weakTo = "Grass";
+        this.strongAgainst = "Water";
+    }
+
+    //methods / unique skills
+    public void thunderbolt() {
+        System.out.println(name + " uses Thunderbolt!");
+    }
+
+    public void voltTackle() {
+        System.out.println(name + " uses VoltTackle!");
+    }
+
+    // override
+    @Override
+    public void plays() {
+        System.out.println(name + " showers sparks in the air!");
+    }
+
+    // getters en setters
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWeakTo() {
+        return weakTo;
+    }
+
+    public void setWeakTo(String weakTo) {
+        this.weakTo = weakTo;
+    }
+
+    public String getStrongAgainst() {
+        return strongAgainst;
+    }
+
+    public void setStrongAgainst(String strongAgainst) {
+        this.strongAgainst = strongAgainst;
+    }
+}
